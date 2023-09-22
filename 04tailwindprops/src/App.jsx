0e902fec03d7f35174ba0302587c24bb449c01b6
varun <singhvarun928@gmail.com>
2023-09-22 07:48:23 +0530

    props

---
+++ 04tailwindprops/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1 className='bg-green-400'>Tailwind test</h1>
+    </>
+  )
+}
+
+export default App
