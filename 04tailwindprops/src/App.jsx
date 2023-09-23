87a1b456a2f698249cf2e2919b7f40a336ce8e6d
varun <singhvarun928@gmail.com>
2023-09-23 21:33:07 +0530

    tailwind practice

--- 04tailwindprops/src/App.jsx
+++ 04tailwindprops/src/App.jsx
@@ -5,9 +5,14 @@ import './App.css'
 function App() {
   return (
     <>
-     <h1 className='bg-green-400'>Tailwind test</h1>
+      <h1 className='bg-green-400 rounded-xl p-4'>Tailwind test</h1>
+
+      <h1 className="font-bold underline text-blue-700/75">
+        Hello world!
+      </h1>
+      <p className='p-4 bg-orange-400 rounded-xl m-4'>React + Tailwind is amazing</p>
     </>
   )
 }
 
-export default App
+export default App;
