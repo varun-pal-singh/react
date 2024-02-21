57a83c0263527cc74509ad2556d93200d468185b
Varun Pal Singh <singhvarun928@gmail.com>
2024-02-21 20:43:02 +0530

    hooks

---
+++ 03-counter-project/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [value, setValue] = useState(0)
+
+  function handleAddClick(){
+    if (value >= 10) return
+    value += 1
+    setValue(value)
+  }
+
+  function handleRemoveClick(){
+    if (value <= 0)  return
+    value -= 1
+    setValue(value)
+  }  
+
+  return (
+    <>
+      <h1>Chai aur react {value}</h1>
+      <h2>Counter value : {value}</h2>
+      <p>current value : {value}</p>
+      <button onClick={() => handleAddClick(value)} >Add 1</button> <br />
+      <button onClick={() => handleRemoveClick(value)}>Remove 1</button>
+      <footer> current value : {value}</footer>
+    </>
+  )
+}
+
+export default App
