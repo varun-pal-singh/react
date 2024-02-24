e0be6e77a9b8f42e066175133b2092167abe6145
Varun Pal Singh <singhvarun928@gmail.com>
2024-02-25 02:09:01 +0530

    react router

---
+++ 08-react-router/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {Header, Footer, Home, About} from './components/index'
+
+function App() {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
---
+++ 08-react-router/src/components/index.js
@@ -0,0 +1,9 @@
+import Header from "./header/Header";
+import Footer from './footer/Footer'
+import About from "./about/About";
+import Home from "./home/Home";
+import Contact from './contact/Contact'
+import User from "./user/User";
+import Github from "./github/Github";
+
+export {Header, Footer, About, Contact, Home, User, Github}
