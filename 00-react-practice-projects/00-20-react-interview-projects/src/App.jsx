83c0d6ee449d5bb58694c0f0ecabb7e97e41a92c
Varun Pal Singh <singhvarun928@gmail.com>
2024-03-05 21:54:38 +0530

    done 2 react projects

---
+++ 00-react-practice-projects/00-20-react-interview-projects/src/App.jsx
@@ -0,0 +1,12 @@
+import Accordion from "./components/Accordion"
+import RandomColor from "./components/RandomColor"
+
+export default function App() {
+  return (
+    <>
+    {/* <Accordion /> */}
+
+    <RandomColor />
+    </>                    
+  )
+}
